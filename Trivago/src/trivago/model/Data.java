df4438ac442149cdae056a41fd4feda19ebf1d7d
Hiruma252 <38736212+Hiruma252@users.noreply.github.com>
2018-10-25 07:42:26 -0300

    ajustes

--- Trivago/src/trivago/model/Data.java
+++ Trivago/src/trivago/model/Data.java
@@ -3,6 +3,8 @@ package trivago.model;
 import trivago.model.bd.Conexion;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import trivago.model.Hotel;
 import trivago.model.bd.Conexion;
 
@@ -15,7 +17,7 @@ public class Data {
     }
 
     public Hotel getHotel(String nombre) throws SQLException {
-        String query = "SELECT * FROM hotel WHERE nombre = '"+nombre+"'";
+        String query = "SELECT * FROM hotel WHERE nombre = '" + nombre + "'";
         Hotel nom = null;
         ResultSet rs = con.ejecutar(query);
         if (rs.next()) {
@@ -31,8 +33,9 @@ public class Data {
 
         return nom;
     }
+
     public Hotel getCiudad(String ciudad) throws SQLException {
-        String query = "SELECT * FROM hotel WHERE ciudad = '"+ciudad+"'";
+        String query = "SELECT * FROM hotel WHERE ciudad = '" + ciudad + "'";
         Hotel ciu = null;
         ResultSet rs = con.ejecutar(query);
         if (rs.next()) {
@@ -48,6 +51,7 @@ public class Data {
 
         return ciu;
     }
+
     public void crearHotel(Hotel h) throws SQLException {
         String insert = "INSERT INTO hotel VALUES "
                 + "(NULL, '" + h.getNombre() + "',"
@@ -55,4 +59,38 @@ public class Data {
                 + "'" + h.getPrecio_por_noche() + "')";
         con.ejecutar(insert);
     }
+
+    public List<Hotel> getHotel() throws SQLException {
+        List<Hotel> lista = new ArrayList<>();
+
+        String query = "SELECT * FROM hotel";
+        ResultSet rs = con.ejecutar(query);
+
+        while (rs.next()) {
+
+            Hotel hot = new Hotel();
+
+            hot.setId(rs.getInt(1));
+            hot.setNombre(rs.getString(2));
+            hot.setCiudad(rs.getString(3));
+            hot.setPrecio_por_noche(rs.getInt(4));
+
+            lista.add(hot);
+        }
+        
+        con.close();
+        
+        return lista;
+    }
+    
+    public List<Hotel> getHotel(String filtro){
+        List<Hotel> lista = new ArrayList<>();
+        
+        String query = "SELECT * FROM hotel WHERE(nombre LIKE"
+                + "'%"+filtro+"%' OR ciudad LIKE '%"+filtro"%')";
+        ResultSet rs = con.ejecutar(query);
+        
+        
+    }
+        
 }
